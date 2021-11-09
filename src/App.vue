545e2218a065c5bb6ec2f210eb415ce485127f10
V.Paparidis <v.paparidis@gmail.com>
2021-11-09 17:26:31 +0200

    hero section work in progress

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,128 @@
 <template>
     <div id="app">
+        <particles
+            id="tsparticles"
+            class="particles"
+            :options="particleOptions"
+        />
         <router-view />
     </div>
 </template>
 
+<script>
+    export default {
+        data() {
+            return {
+                particleOptions: {
+                    fullScreen: {
+                        zIndex: 1
+                    },
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: 'repulse'
+                            }
+                        },
+                        modes: {
+                            bubble: {
+                                distance: 500,
+                                duration: 2,
+                                opacity: 0.8,
+                                size: 40
+                            }
+                        }
+                    },
+                    particles: {
+                        color: {
+                            value: 'fff'
+                        },
+                        links: {
+                            color: {
+                                value: 'hsl(0, 0%, 30%)'
+                            },
+                            distance: 200,
+                            opacity: 0.1,
+                            width: 1
+                        },
+                        move: {
+                            attract: {
+                                rotate: {
+                                    x: 600,
+                                    y: 1200
+                                }
+                            },
+                            enable: true,
+                            outModes: {
+                                bottom: 'out',
+                                left: 'out',
+                                right: 'out',
+                                top: 'out'
+                            },
+                            speed: 1
+                        },
+                        number: {
+                            density: {
+                                enable: true
+                            },
+                            limit: 500,
+                            value: 250
+                        },
+                        opacity: {
+                            value: 1,
+                            animation: {
+                                speed: 1,
+                                minimumValue: 0.1
+                            }
+                        },
+                        size: {
+                            value: 1,
+                            animation: {
+                                speed: 30,
+                                minimumValue: 0.1
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
 <style>
+@import '~@/global.css';
+
+#app {
+    background: url('~@/assets/hero-image.jpg');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    box-shadow: inset 0 0 0 1000px hsla(266, 31%, 37%, 0.4);
+}
+
 * {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    font-family: var(--font-headers);
+    font-weight: bold;
+    text-shadow: var(--text-shadow);
+}
+
+html,
+body {
+    font-family: var(--font-body);
+    text-shadow: var(--text-shadow);
 }
 </style>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="hello">
-        <h1 class="route-title">Hello World</h1>
-    </div>
+    <section class="hero-section">
+        <div class="info-container">
+            <h1 class="route-title"> &lt;FRONT-END <br>ENGINEER &lt;&#47;&gt;</h1>
+            <p>Currently available for contract only</p>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -11,7 +14,53 @@
 </script>
 
 <style scoped>
-.route-title {
+.hero-section {
     color: #fff;
+    position: relative;
+    min-height: 100vh;
+    width: 90%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    margin: 0 auto;
 }
+
+h1 {
+    font-size: 3.4rem;
+    margin-bottom: 1rem;
+    user-select: none;
+}
+
+p {
+    font-size: 1.1rem;
+    padding-left: 0.5rem;
+    user-select: none;
+}
+
+@media screen and (min-width: 768px) {
+    h1 {
+        font-size: 6rem;
+    }
+
+    p {
+        font-size: 1.4rem;
+    }
+}
+
+@media screen and (min-width: 1024px) {
+    .hero-section {
+        width: 85%;
+    }
+
+    h1 {
+        font-size: 10rem;
+    }
+
+    p {
+        font-size: 1.5rem;
+        padding-left: 1rem;
+    }
+}
+
 </style>
