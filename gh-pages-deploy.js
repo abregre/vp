7df2c1a3bb7d7012e1ef79eb110f34b3301fd49f
V.Paparidis <v.paparidis@gmail.com>
2021-11-11 18:46:50 +0200

    automatic deploy

--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 (async () => {
     try {
         await execa('git', ['checkout', '--orphan', 'gh-pages'])
+        // eslint-disable-next-line no-console
         console.log('Building started...')
         await execa('npm', ['run', 'build'])
         const folderName = fs.existsSync('dist') ? 'dist' : 'build'
@@ -17,6 +18,7 @@ const fs = require('fs');
         await execa('git', ['branch', '-D', 'gh-pages'])
         console.log('Successfully deployed, check your settings')
     } catch (e) {
+        // eslint-disable-next-line no-console
         console.log(e.message)
         process.exit(1)
     }
