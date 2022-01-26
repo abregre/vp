7fccc75224d660f012256b32c97d42ea34f908ed
V.Paparidis <v.paparidis@gmail.com>
2022-01-27 00:31:10 +0200

    remove emailjs

--- src/main.js
+++ src/main.js
@@ -2,8 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { init } from '@emailjs/browser'
-
 import VueParticles from 'particles.vue'
 import VueScrollTo from 'vue-scrollto'
 import VueDOMPurifyHTML from 'vue-dompurify-html'
@@ -12,7 +10,6 @@ Vue.use(VueDOMPurifyHTML)
 
 Vue.use(VueParticles)
 Vue.use(VueScrollTo)
-init('user_ZDzKQhpPRR139kexvPV1o')
 Vue.config.productionTip = false
 
 new Vue({
